f8ed018b4a3b14a54efddea368f4e6bbf346ba3b
Frederik Ring <frederik.ring@gmail.com>
2024-12-20 19:20:19 +0100

    Run code given in textarea

--- src/main.js
+++ src/main.js
@@ -4,6 +4,9 @@ createApp({
   data() {
     return {
       pyodide: null,
+      script: '',
+      requirements: '',
+      output: ['Output goes here ...']
     }
   },
   computed: {
@@ -16,10 +19,39 @@ createApp({
   },
   async mounted() {
     try {
-      const pyodide = await window.loadPyodide()
+      const pyodide = await window.loadPyodide({
+        env: {
+          'FILES_PATH': '/home/pyodide/pyla'
+        }
+      })
       this.pyodide = pyodide
+      this.pyodide.setStdout({ batched: (msg) => this.output.push(msg) })
     } catch (err) {
       this.pyodide = err
+    }
+  },
+  methods: {
+    async run () {
+      if (this.requirements.trim()) {
+        const requirements = this.requirements.trim().split('\n')
+        await this.pyodide.loadPackage('micropip')
+            .then(() => this.pyodide.pyimport('micropip'))
+            .then(async micropip => {
+              for (const req of requirements) {
+                await micropip.install(req)
+              }
+            })
+      }
+      const dirHandle = await showDirectoryPicker()
+      const permissionStatus = await dirHandle.requestPermission({
+        mode: 'readwrite',
+      })
+
+      if (permissionStatus !== 'granted') {
+        throw new Error('read access to directory not granted')
+      }
+      const nativefs = await this.pyodide.mountNativeFS('/home/pyodide/pyla', dirHandle)
+      await this.pyodide.runPython(this.script)
     }
   }
 }).mount('#app')
