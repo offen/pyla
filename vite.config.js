2b3f1dde3da0f131b7eacc869a247a78085158ea
Frederik Ring <frederik.ring@gmail.com>
2024-12-20 12:24:07 +0100

    Asset base is missing

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '/pyla/',
   resolve: {
     alias: {
       vue: 'vue/dist/vue.esm-bundler.js',
