803af462cb53e15a67fd9cdf74c8fcc473bab085
Frederik Ring <frederik.ring@gmail.com>
2024-12-20 16:24:15 +0100

    Load and initialized pyodide on mount

--- src/main.js
+++ src/main.js
@@ -3,7 +3,23 @@ import { createApp } from 'vue'
 createApp({
   data() {
     return {
-      'name': 'Pyla'
+      pyodide: null,
+    }
+  },
+  computed: {
+    globalError() {
+      if (this.pyodide instanceof Error) {
+        return this.pyodide
+      }
+      return null
+    }
+  },
+  async mounted() {
+    try {
+      const pyodide = await window.loadPyodide()
+      this.pyodide = pyodide
+    } catch (err) {
+      this.pyodide = err
     }
   }
 }).mount('#app')
