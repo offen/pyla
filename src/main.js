f2d0cd25a24751151e39a44684eb892453fcfb56
Frederik Ring <frederik.ring@gmail.com>
2024-12-21 16:03:39 +0100

    Event handler does not propagate errors yet

--- src/main.js
+++ src/main.js
@@ -6,14 +6,18 @@ createApp({
       pyodide: null,
       script: '',
       requirements: '',
-      output: ['Output goes here ...'],
-      filesPath: '/home/pyodide/pyla'
+      output: [],
+      filesPath: '/home/pyodide/pyla',
+      runtimeError: null
     }
   },
   computed: {
     globalError() {
       if (this.pyodide instanceof Error) {
         return this.pyodide
+      }
+      if (this.runtimeError) {
+        return this.runtimeError
       }
       return null
     },
@@ -36,36 +40,38 @@ createApp({
   },
   methods: {
     async run () {
-      this.output = []
-
-      if (this.requirements.trim()) {
-        const requirements = this.requirements.trim().split('\n')
-        await this.pyodide.loadPackage('micropip')
-            .then(() => this.pyodide.pyimport('micropip'))
-            .then(async micropip => {
-              for (const req of requirements) {
-                await micropip.install(req)
-              }
-            })
-      }
-
-      let nativefs
-      if (this.isUsingFilesystem) {
-        const dirHandle = await showDirectoryPicker()
-        const permissionStatus = await dirHandle.requestPermission({
-          mode: 'readwrite',
-        })
-
-        if (permissionStatus !== 'granted') {
-          throw new Error('read access to directory not granted')
+      try {
+        if (this.requirements.trim()) {
+          const requirements = this.requirements.trim().split('\n')
+          await this.pyodide.loadPackage('micropip')
+              .then(() => this.pyodide.pyimport('micropip'))
+              .then(async micropip => {
+                for (const req of requirements) {
+                  await micropip.install(req)
+                }
+              })
         }
-        nativefs = await this.pyodide.mountNativeFS(this.filesPath, dirHandle)
-      }
 
-      await this.pyodide.runPython(this.script)
+        let nativefs
+        if (this.isUsingFilesystem) {
+          const dirHandle = await showDirectoryPicker()
+          const permissionStatus = await dirHandle.requestPermission({
+            mode: 'readwrite',
+          })
 
-      if (nativefs) {
-        await nativefs.syncfs()
+          if (permissionStatus !== 'granted') {
+            throw new Error('read access to directory not granted')
+          }
+          nativefs = await this.pyodide.mountNativeFS(this.filesPath, dirHandle)
+        }
+
+        await this.pyodide.runPython(this.script)
+
+        if (nativefs) {
+          await nativefs.syncfs()
+        }
+      } catch (err) {
+        this.runtimeError = err
       }
     }
   }
