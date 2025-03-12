ec97d17916ec1582ba62c28e253079b812e6a2b7
Frederik Ring <frederik.ring@gmail.com>
2025-03-12 20:47:58 +0100

    Path for subdomain deployment is shifted

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), tailwindcss()],
-  base: '/pyla/',
+  base: '/',
   resolve: {
     alias: {
       vue: 'vue/dist/vue.esm-bundler.js',
