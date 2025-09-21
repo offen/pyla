f59ecfa8cb755f4d36d14310820b8d71a09b9e2a
Frederik Ring <frederik.ring@gmail.com>
2025-09-21 17:21:01 +0200

    Configuration is expressed in dedicated struct

--- server/cmd/server/main.go
+++ server/cmd/server/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"log"
 	"net/http"
@@ -9,33 +10,19 @@ import (
 	"net/url"
 	"os"
 	"os/signal"
+	"strconv"
 	"strings"
 	"syscall"
 	"time"
 )
 
 func main() {
-	ghModelsPAT := os.Getenv("GITHUB_MODELS_PAT")
-	if ghModelsPAT == "" {
-		log.Fatal("GITHUB_MODELS_PAT environment variable not set")
+	c, err := newCfg()
+	if err != nil {
+		log.Fatal(err)
 	}
 
-	ghModelsURL := os.Getenv("GITHUB_MODELS_URL")
-	if ghModelsURL == "" {
-		log.Fatal("GITHUB_MODELS_URL environment variable not set")
-	}
-
-	fairUseToken := os.Getenv("FAIR_USE_TOKEN")
-	if fairUseToken == "" {
-		log.Fatal("FAIR_USE_TOKEN environment variable not set")
-	}
-
-	port := os.Getenv("PORT")
-	if port == "" {
-		log.Fatal("PORT environment variable not set")
-	}
-
-	proxyHandler, err := getProxy(ghModelsPAT, ghModelsURL, fairUseToken)
+	proxyHandler, err := getProxy(c)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -45,12 +32,12 @@ func main() {
 	mux.Handle("/", http.FileServer(http.Dir("/var/www/html/pyla")))
 
 	srv := &http.Server{
-		Addr:    fmt.Sprintf("0.0.0.0:%s", port),
+		Addr:    fmt.Sprintf("0.0.0.0:%d", c.port),
 		Handler: &mux,
 	}
 
 	go srv.ListenAndServe()
-	log.Printf("Server now listening on port %s", port)
+	log.Printf("Server now listening on port %d", c.port)
 
 	quit := make(chan os.Signal, 1)
 	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
@@ -65,23 +52,62 @@ func main() {
 	log.Print("Gracefully shut down server")
 }
 
-func getProxy(ghModelsPAT, ghModelsURL, fairUseToken string) (http.Handler, error) {
-	remote, err := url.Parse(ghModelsURL)
-	if err != nil {
-		return nil, fmt.Errorf("Error parsing given URL: %w", err)
-	}
-
-	proxy := httputil.NewSingleHostReverseProxy(remote)
+func getProxy(c *cfg) (http.Handler, error) {
+	proxy := httputil.NewSingleHostReverseProxy(c.inferenceUrl)
 
 	originalDirector := proxy.Director
 	proxy.Director = func(req *http.Request) {
 		originalDirector(req)
-		if strings.HasSuffix(req.Header.Get("Authorization"), fairUseToken) {
+		if strings.HasSuffix(req.Header.Get("Authorization"), c.fairUseToken) {
 			log.Print("Proxying inference request using fair use token")
-			req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", ghModelsPAT))
+			req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", c.accessToken))
 		}
-		req.Host = remote.Host
+		req.Host = c.inferenceUrl.Host
 	}
 
 	return http.HandlerFunc(proxy.ServeHTTP), nil
+}
+
+type cfg struct {
+	accessToken  string
+	inferenceUrl *url.URL
+	fairUseToken string
+	port         int
+}
+
+func newCfg() (*cfg, error) {
+	ghModelsPAT := os.Getenv("GITHUB_MODELS_PAT")
+	if ghModelsPAT == "" {
+		return nil, errors.New("GITHUB_MODELS_PAT environment variable not set")
+	}
+
+	ghModelsURL := os.Getenv("GITHUB_MODELS_URL")
+	if ghModelsURL == "" {
+		return nil, errors.New("GITHUB_MODELS_URL environment variable not set")
+	}
+	asURL, err := url.Parse(ghModelsURL)
+	if err != nil {
+		return nil, fmt.Errorf("Failed to parse given URL string: %w", err)
+	}
+
+	fairUseToken := os.Getenv("FAIR_USE_TOKEN")
+	if fairUseToken == "" {
+		return nil, errors.New("FAIR_USE_TOKEN environment variable not set")
+	}
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		return nil, errors.New("PORT environment variable not set")
+	}
+	asInt, err := strconv.Atoi(port)
+	if err != nil {
+		return nil, fmt.Errorf("Failed to parse given port value to int: %w", err)
+	}
+
+	return &cfg{
+		accessToken:  ghModelsPAT,
+		inferenceUrl: asURL,
+		fairUseToken: fairUseToken,
+		port:         asInt,
+	}, nil
 }
