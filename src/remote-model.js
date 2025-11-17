ffbc334ad612959bc92f33c40e7f25b8af105cfa
Hendrik Niefeld <hello@niefeld.com>
2025-11-17 20:45:18 +0100

    roll back to gpt-4.1 (#13)

--- src/remote-model.js
+++ src/remote-model.js
@@ -3,7 +3,7 @@ import OpenAI from 'openai'
 const FAIR_USE_TOKEN = 'pyla-fair-use'
 
 export default class RemoteModel {
-  constructor(token = FAIR_USE_TOKEN, model = 'openai/gpt-4.1-mini') {
+  constructor(token = FAIR_USE_TOKEN, model = 'openai/gpt-4.1') {
     this.client = new OpenAI({
       baseURL: `${window.location.origin}/inference`,
       apiKey: token,
