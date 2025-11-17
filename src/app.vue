b44efda061c4269037d4cd39462a1f62bf3b29ea
hendr-ik <hello@niefeld.com>
2025-11-17 10:28:57 +0100

    header link, layout update

--- src/app.vue
+++ src/app.vue
@@ -132,6 +132,9 @@ export default {
       this.token = null
       window.localStorage.removeItem('pat_models_token_v1')
     },
+    reloadPage() {
+      window.location.reload()
+    },
     clearAll() {
       this.script = ''
       this.prompt = ''
@@ -266,15 +269,17 @@ export default {
 </script>
 
 <template>
+  <div id="container" class="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-8 gap-4">
 
-  <div v-if="pyodide" id="container" class="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-8 gap-4">
-
-    <div class="order-1 col-span-2 md:col-span-2 lg:col-span-1 self-center font-semibold text-2xl">
-      <h1>
-        Pyla
-      </h1>
+    <div class="order-1 col-span-2 md:col-span-2 lg:col-span-1 self-center">
+      <a href="#" @click.prevent="reloadPage">
+        <h1 class="font-semibold text-2xl inline-block">
+          Pyla
+        </h1>
+      </a>
     </div>
 
+    <template v-if="pyodide">
     <div
       class="order-2 md:order-2 lg:order-3 col-span-2 lg:col-span-7 self-center flex flex-row justify-end items-center space-x-2">
       <Button type="outline" @click="clearAll">
@@ -452,28 +457,26 @@ export default {
       </div>
     </div>
 
-  </div>
+    </template>
+    <template v-else>
+      <div class="order-2 col-span-2 md:col-span-4 lg:col-span-7 lg:col-start-2 flex flex-row items-center">
+        <div
+          class="w-10 h-10 outline-2 outline-neutral-400 bg-neutral-400 rounded-lg flex items-center justify-center ml-6 md:ml-0">
+          <svg class="size-5 animate-spin text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+            <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+            <path class="opacity-75" fill="currentColor"
+              d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z">
+            </path>
+          </svg>
+        </div>
+        <div class="h-10 text-neutral-500 flex items-center ml-6">
+          <p>
+            Python runtime is initializing ...
+          </p>
+        </div>
+      </div>
+    </template>
 
-  <div v-else class="order-9 flex flex-row items-center">
-    <div class="font-semibold text-2xl">
-      <h1>
-        Pyla
-      </h1>
-    </div>
-    <div
-      class="w-10 h-10 outline-2 outline-neutral-400 bg-neutral-400 rounded-lg flex items-center justify-center ml-6">
-      <svg class="size-5 animate-spin text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-        <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
-        <path class="opacity-75" fill="currentColor"
-          d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z">
-        </path>
-      </svg>
-    </div>
-    <div class="h-10 text-neutral-500 flex items-center ml-6">
-      <p>
-        Python runtime is initializing ...
-      </p>
-    </div>
   </div>
 
   <footer class="mb-2 mt-20 text-neutral-500">
